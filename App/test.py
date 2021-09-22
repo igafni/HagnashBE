bdbd8b615ee78f671fd2a7421249b1e0f238c26a
Itay Gafni <Igafni21@gmail.com>
2021-09-23 02:32:45 +0300

    adding images

--- App/test.py
+++ App/test.py
@@ -3,33 +3,41 @@ import uuid
 
 es = ElasticAdapter(connection_string=CONNECTION_STRING, api_key=API_KEY, index="places")
 es.connect()
-# res = {
-#     "id": uuid.uuid4(),
-#     "name": "כפר עציון",
-#     "description": "יישוב ממש נחמד",
-#     "images": [{
-#         "path": "static/images/906c4c97-d129-498b-a49a-815e78a8f7e1.jpg",
-#         "description": "היישוב"
-#     }],
-#     "area": "גוש עציון",
-#     "location": {"lon": "31.6492899", "lat": "35.1106856"},
-#     "facilities": ["music_room"],
-#     "guard_post": [{"type": "sit", "name": "שג",
-#                     "description": "יש מזגן טוב"},
-#                    {"type": "walk", "name": "פטרול",
-#                     "description": "אין גדר אז צריך להיות חדים בלילה"}
-#                    ]
-# }
-# es.index_document(res)
-# res1 = es.search_document({"query": {
-#     "query_string": {
-#         "query": "*בית*"
-#     }
-# }})
-res = es.search_document({"query": {
-        "match": {
-            "id": "56d020b5-e2c2-4c3c-8f97-4d6784b4ece4"
+res = {
+    "id": uuid.uuid4(),
+    "name": "עתניאל",
+    "description": "יש גדר אז אין יותר מדי על מה לשמור",
+    "images": [
+      {
+        "path": "static/images/4afe1868-82f3-43b3-b4b0-71a083e3b359.jpg",
+        "description": "ברוכים הבאים"
+      },
+        {
+        "path": "static/images/d8a1fe3f-861c-4cff-ac7c-246f9610f720.jpg",
+        "description": "עושים כיף"
         }
-    }})
-print(res['hits']['hits'][0]['_source'])
+    ],
+    "area": "יהודה",
+    "location": {
+      "lon": "31.4388597",
+      "lat": "35.0223552"
+    },
+    "facilities": [
+      "music_room"
+    ],
+    "guard_post": [
+      {
+        "type": "sit",
+        "name": "שג",
+        "description": "לפעמים הרבשצ בודק"
+      }
+    ]
+  }
+
+es.index_document(res)
+
+
+#print(res['hits']['hits'][0]['_source'])
+print(uuid.uuid4())
+
 es.close()
